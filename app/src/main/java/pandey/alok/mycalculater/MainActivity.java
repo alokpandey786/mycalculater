f569ca7d3d4074c38ce6031e4f1ae83a4fd50ea6
Alokpandey786 <pandayalok786@gmail.com>
2019-01-17 11:00:28 +0530

    init commit

---
+++ app/src/main/java/pandey/alok/mycalculater/MainActivity.java
@@ -0,0 +1,13 @@
+package pandey.alok.mycalculater;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
